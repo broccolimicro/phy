4da42982bfed1c14814c27782355c84d485c2239
Ned Bingham <edward.bingham@broccolimicro.io>
2024-09-16 09:52:48 -0400

    removing all complex argument handling. That's taken care of before we even receive the string

--- phy/Script.cpp
+++ phy/Script.cpp
@@ -1,115 +1,7 @@
 #include "Script.h"
 
-#include <dirent.h>
-#include <fnmatch.h>
-#include <pwd.h>
-
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
-
-std::string expandTilde(const std::string& input) {
-	if (input[0] == '~') {
-		std::string homeDir;
-
-		if (input.length() == 1 || input[1] == '/') {
-			// If the tilde is followed by a slash or it's just `~`, use the current user's home directory
-			const char* home = getenv("HOME");
-			if (!home) {
-				struct passwd* pw = getpwuid(getuid());
-				homeDir = pw->pw_dir;
-			} else {
-				homeDir = home;
-			}
-		}
-
-		return homeDir + input.substr(1);
-	}
-	return input;
-}
-
-std::string expandEnvVariables(const std::string& input) {
-	std::string output;
-	size_t pos = 0;
-	bool inEscape = false;
-
-	while (pos < input.size()) {
-		if (inEscape) {
-			// If we are in escape mode, just append the character literally
-			output += input[pos];
-			inEscape = false;
-			pos++;
-			continue;
-		}
-
-		if (input[pos] == '\\') {
-			// Escape sequence (e.g., \$), skip the backslash and escape the next char
-			inEscape = true;
-			pos++;
-		} else if (input[pos] == '$') {
-			size_t endPos = pos + 1;
-
-			if (endPos < input.size() && input[endPos] == '{') {
-				// Handle ${VAR} syntax
-				endPos++;  // Skip past '{'
-				size_t varEnd = input.find('}', endPos);
-				if (varEnd != std::string::npos) {
-					std::string varName = input.substr(endPos, varEnd - endPos);
-					const char* varValue = getenv(varName.c_str());
-
-					if (varValue) {
-						output += varValue;
-					}
-					pos = varEnd + 1;  // Move past '}'
-					continue;
-				}
-			} else {
-				// Handle $VAR syntax (without braces)
-				while (endPos < input.size() && (isalnum(input[endPos]) || input[endPos] == '_')) {
-					endPos++;
-				}
-
-				std::string varName = input.substr(pos + 1, endPos - pos - 1);
-				const char* varValue = getenv(varName.c_str());
-
-				if (varValue) {
-					output += varValue;
-				}
-
-				pos = endPos;
-				continue;
-			}
-		}
-
-		// Append literal characters
-		output += input[pos];
-		pos++;
-	}
-
-	return output;
-}
-
-std::vector<std::string> globFiles(const std::string& pattern) {
-	std::vector<std::string> matches;
-	std::string dirPath = ".";
-
-	size_t lastSlash = pattern.find_last_of('/');
-	if (lastSlash != std::string::npos) {
-		dirPath = pattern.substr(0, lastSlash);
-	}
-
-	std::string filePattern = pattern.substr(lastSlash + 1);
-	DIR* dir = opendir(dirPath.c_str());
-	if (dir == nullptr) return matches;
-
-	struct dirent* entry;
-	while ((entry = readdir(dir)) != nullptr) {
-		if (fnmatch(filePattern.c_str(), entry->d_name, 0) == 0) {
-			matches.push_back(dirPath + "/" + entry->d_name);
-		}
-	}
-	closedir(dir);
-	return matches;
-}
 
 std::vector<std::string> splitArguments(const std::string& input) {
 	std::vector<std::string> args;
@@ -145,25 +37,6 @@ std::vector<std::string> splitArguments(const std::string& input) {
 	}
 
 	return args;
-}
-
-std::vector<std::string> wordExpand(const std::string& input) {
-	std::vector<std::string> args = splitArguments(input);
-	std::vector<std::string> expandedArgs;
-
-	for (auto& arg : args) {
-		arg = expandTilde(arg);
-		arg = expandEnvVariables(arg);
-
-		if (arg.find('*') != std::string::npos || arg.find('?') != std::string::npos) {
-			std::vector<std::string> globbedFiles = globFiles(arg);
-			expandedArgs.insert(expandedArgs.end(), globbedFiles.begin(), globbedFiles.end());
-		} else {
-			expandedArgs.push_back(arg);
-		}
-	}
-
-	return expandedArgs;
 }
 
 namespace phy {
@@ -375,7 +248,7 @@ bool loadTech(Tech &dst, string path) {
 	PyConfig config;
 	PyConfig_InitPythonConfig(&config);
 
-	vector<string> args = wordExpand(path);
+	vector<string> args = splitArguments(path);
 	vector<char*> argv;
 	for (auto arg = args.begin(); arg != args.end(); arg++) {
 		argv.push_back(const_cast<char*>(arg->c_str()));
